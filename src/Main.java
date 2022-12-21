eb2a82d8bb53cb339976d28105d6d5d8c57e041d
Людок <skobeleva.ludmila@yandex.ru>
2022-12-21 23:59:32 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+       Set <String> set= new HashSet<>();
+       set.add("банан");
+       set.add("молоко");
+       set.add("хлеб");
+       set.add("кукуруза");
+       set.add("банан");
+       set.add("яблоки");
+       set.add("курица");
+       set.add("банан");
 
 
 
-        System.out.println("Hello world!");
+
+        System.out.println(Arrays.toString(set.toArray(new String[0])));
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,69 @@
+import java.util.HashSet;
+import java.util.Objects;
+
 public class Product {
+    String productName;
+    int productPrice;
+    int productQuantity;
+
+    public Product(String productName, int productPrice, int productQuantity) {
+        setProductName(productName);
+        setProductPrice(productPrice);
+        setProductQuantity(productQuantity);
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        if (productName==null||productName.isEmpty()){
+            throw new IllegalArgumentException(" Заполните карточку товара полностью ");
+        }
+        this.productName = productName;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        if (productPrice<=0){
+            throw new IllegalArgumentException(" Заполните карточку товара полностью ");
+        }
+        this.productPrice = productPrice;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        if (productQuantity<=0){
+            throw new IllegalArgumentException(" Заполните карточку товара полностью ");
+        }
+        this.productQuantity = productQuantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return getProductName().equals(product.getProductName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getProductName());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", productPrice=" + productPrice +
+                ", productQuantity=" + productQuantity +
+                '}';
+    }
 }
