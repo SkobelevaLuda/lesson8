139f3f99c2a0e079c471174b8b33da3641d7608c
Людок <skobeleva.ludmila@yandex.ru>
2022-12-22 00:48:24 +0300

    Задание 1, 2.1, 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-       Set <String> set= new HashSet<>();
+        Random random = new Random();
+        Set<Integer> nambers = new HashSet<>();
+        while (nambers.size() < 20) {
+            nambers.add(random.nextInt(1000));
+        }
+        Iterator<Integer> iterator = nambers.iterator();
+        while (iterator.hasNext()) {
+            Integer number = iterator.next();
+            if (number % 2 == 1) {
+                iterator.remove();
+            }
+        }
+        System.out.println(nambers);
+
+       /*Set <String> set= new HashSet<>();
        set.add("банан");
        set.add("молоко");
        set.add("хлеб");
@@ -15,9 +29,8 @@ public class Main {
        set.add("курица");
        set.add("банан");
 
-
-
-
         System.out.println(Arrays.toString(set.toArray(new String[0])));
+    }*/
     }
+
 }
